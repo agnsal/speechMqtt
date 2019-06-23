b4a172536f41f6b7e6f84d7057cf242d712fcb4b
Agnese Salutari <agneses92@hotmail.it>
2019-06-23 21:33:57 +0200

    Update upythonDeviceSub.py

--- upythonDeviceSub.py
+++ upythonDeviceSub.py
@@ -19,7 +19,7 @@ def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("myDevice/commands")
+    client.subscribe(subTopic)
 
 
 def on_message(client, userdata, msg):
